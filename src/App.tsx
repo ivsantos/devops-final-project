e8aafa502c82a760a249936ccc252c1bed6bc91a
Ivan Santos Panadero <ivan.santos.sge@mango.com>
2023-06-23 21:16:08 +0200

    feat: :loud_sound: Add Sentry as error tracker and perf monitoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import Confetti from 'react-confetti';
-import styles from './App.module.css';
+import styles from './App.module.scss';
+import { useState } from 'react';
+
+function BrokenComponent() {
+  throw new Error('Broken component');
+}
 
 export default function App() {
+  const [error, setError] = useState(false);
+  const throwKnownError = () => {
+    throw new Error('✋ This is a known error that will be caught by Sentry');
+  };
+
+  const handleLoadBrokenComponent = () => {
+    setError(true);
+  };
+
   return (
     <>
-      <h1 className={styles.title}>Good night! 🌃😴🐱</h1>
+      <h1 className={styles.title}>Hello! 🐱</h1>
+      <button onClick={() => throwKnownError()}>🔴 Throw controlled error 🔴</button>
+      <button className={styles.fireButton} onClick={handleLoadBrokenComponent}>
+        🔥 Break the app 🔥
+      </button>
+      {error && <BrokenComponent />}
       <Confetti recycle={false} />
     </>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: 'https://daf52c8ef4904f7fb3295af059116c4b@o4505410145157120.ingest.sentry.io/4505410171895808',
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ['localhost', /^\//],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 0.7,
+  // Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
